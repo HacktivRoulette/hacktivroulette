dcb5f546d21fb18cf80d2ce0ca793743552d52b3
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-07-26 19:16:39 +0700

    third-commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,11 +4,14 @@
       <div id="cont" class="row center">
         <div id="contt" class="col s12 m6 l6 center">
           <div class="row">
-            <div id="conttt" class="col s12 m12 l12">
-              <h1>ini button</h1>
+            <div  class="col s12 m12 l12">
+              <i style="color:white" class="material-icons prefix">account_circle</i>
+              <input style="color:white" value="Nickname..." id="icon_prefix" type="text" class="validate">
+              
             </div>
             <div id="conttt" class="col s12 m12 l12">
-              <button > Submit</button>
+              <a >Submit</a>
+
             </div>
           </div>
 
