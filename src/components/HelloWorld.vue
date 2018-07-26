00b641b0311cf9143580c526396fe174f010cd3f
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-07-26 19:20:59 +0700

    fourth-commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,15 +4,20 @@
       <div id="cont" class="row center">
         <div id="contt" class="col s12 m6 l6 center">
           <div class="row">
-            <div  class="col s12 m12 l12">
+            <div class="col s12 m12 l12">
               <i style="color:white" class="material-icons prefix">account_circle</i>
               <input style="color:white" value="Nickname..." id="icon_prefix" type="text" class="validate">
-              
-            </div>
-            <div id="conttt" class="col s12 m12 l12">
-              <a >Submit</a>
 
             </div>
+            <router-link to="/about">
+              <div id="conttt" class="col s12 m12 l12">
+                <h6>
+                  <b style="color:white">Submit</b>
+                </h6>
+
+              </div>
+            </router-link>
+
           </div>
 
         </div>
